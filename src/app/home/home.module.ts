34e2e32c0837f01f78c8a8d56da8dfca417aa941
Deepak Kumar Swain <dpkswn4@gmail.com>
2025-02-04 21:08:55 +0530

    changes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { homeRouting } from './home.route';
-import {HomeComponent} from './home.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {Ng2GoogleChartsModule} from 'ng2-google-charts';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {MatListModule} from '@angular/material/list';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { HomeComponent } from './home.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [HomeComponent],
@@ -18,8 +16,6 @@ import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
     MatCardModule,
     MatButtonModule,
     MatIconModule,
-    Ng2GoogleChartsModule,
-    FlexLayoutModule,
     MatListModule,
     FontAwesomeModule
   ]
